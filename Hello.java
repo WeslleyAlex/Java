7ba5565a871aed28eb34d7d7376b895eb6e7aad8
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-07-29 11:14:46 -0300

    Primeiro commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+	public static void main(String[] args) {
+		
+		//System.out.print("Hello World!");
+		/*System.out.println("A aula esta uma BOSTA!");*/
+		System.out.print("A aula esta uma BOSTA!\nTeste");
+
+	}
+}
